203705fecb11e07c8fc6a4ea5ca32765961caac5
ahmetrifaioglu <ahmetrifaioglu.ce@gmail.com>
2019-01-29 15:49:34 +0000

    updated

--- bin/dream_challenge_analysis_functions.py
+++ bin/dream_challenge_analysis_functions.py
@@ -8,6 +8,62 @@ awk -F ',' '{print $2}' idg_comp_targ_test.csv  > idg_train_test_prot_ids.txt
 awk -F ',' '{print $2}' idg_comp_targ_uniq_inter_filtered.csv  >> idg_train_test_prot_ids.txt
 
 """
+def getAverageRMSEResult():
+    import os
+    import numpy as np
+    file_dict_rmse = dict()
+    file_dict_training_loss = dict()
+    file_dict_val_loss = dict()
+    result_file_path = "../resultFiles"
+    for fl in os.listdir(result_file_path):
+        if fl.startswith("pinn"):
+            fl_id = fl.split("_")[1][0]
+            file_dict_rmse[fl_id] = [[] for _ in range(5)]
+            file_dict_training_loss[fl_id] = [[] for _ in range(5)]
+            file_dict_val_loss[fl_id] = [[] for _ in range(5)]
+
+            fl_result = open("{}/{}".format(result_file_path, fl) , "r")
+            lst_result_fl = fl_result.read().split("\n")
+            fl_result.close()
+            for line in lst_result_fl:
+                if line.startswith("Fold:"):
+                    fold_id, epoch_id, rmse, training_loss, validation_loss = line.split("\t")
+                    fold_id, epoch_id, rmse, training_loss, validation_loss = int(fold_id.split(":")[1]), \
+                                                                              int(epoch_id.split(":")[1]), \
+                                                                              float(rmse.split(":")[1]), \
+                                                                              float(training_loss.split(":")[1]), \
+                                                                              float(validation_loss.split(":")[1])
+                    # print(fold_id, epoch_id, rmse, training_loss, validation_loss)
+                    # print(line)
+                    file_dict_rmse[fl_id][fold_id-1].append(rmse)
+                    file_dict_training_loss[fl_id][fold_id - 1].append(training_loss)
+                    file_dict_val_loss[fl_id][fold_id - 1].append(validation_loss)
+
+    #print(len(file_dict_rmse["1"][4]))
+
+    arr_all_mean_fold_rmse  = []
+    arr_all_mean_fold_training = []
+    arr_all_mean_fold_val = []
+
+    for key in file_dict_rmse.keys():
+        arr_mean_fold_rmse = np.array(file_dict_rmse[key]).mean(axis=0)
+        arr_all_mean_fold_rmse.append(arr_mean_fold_rmse)
+
+        arr_mean_fold_training = np.array(file_dict_training_loss[key]).mean(axis=0)
+        arr_all_mean_fold_training.append(arr_mean_fold_training)
+
+        arr_mean_fold_val = np.array(file_dict_val_loss[key]).mean(axis=0)
+        arr_all_mean_fold_val.append(arr_mean_fold_val)
+
+    arr_all_mean_fold_rmse = np.array(arr_all_mean_fold_rmse).mean(axis=0)
+    arr_all_mean_fold_train = np.array(arr_all_mean_fold_training).mean(axis=0)
+    arr_all_mean_fold_val = np.array(arr_all_mean_fold_val).mean(axis=0)
+
+    for ind in range(len(arr_all_mean_fold_rmse)):
+        print("{}\t{}\t{}\t{}".format(ind, arr_all_mean_fold_rmse[ind], arr_all_mean_fold_train[ind], arr_all_mean_fold_val[ind]))
+    # print(arr_all_mean_fold_rmse)
+
+getAverageRMSEResult()
 def getMissingCompoundTargetIDs():
     fl_prot_ids = open("{}/{}".format(datasets_path, "idg_train_test_prot_ids.txt"))
     lst_prot_ids = fl_prot_ids.read().split("\n")
--- bin/dream_challenge_full_training.py
+++ bin/dream_challenge_full_training.py
@@ -35,9 +35,9 @@ def train_networks(mod, comp_feat, tar_feat, comp_hidden_lst, tar_hidden_lst, fc
     learn_rate = float(lr)
     print(modeltype, comp_feature_list, tar_feature_list, fc1, fc2, learn_rate)
     #learn_rate = sys.argv[2]
-    n_epoch = 20
+    n_epoch = 100
     num_of_folds = 1
-    batch_size = 32
+    batch_size = 64
 
     comp_tar_pair_dataset = "idg_comp_targ_uniq_inter_filtered.csv"
     comp_tar_pair_test_dataset = "idg_comp_targ_test.csv"
