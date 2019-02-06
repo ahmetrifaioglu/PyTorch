dc223f56bdeb7120dd83e779f5ca2e357b792509
ahmetrifaioglu <ahmetrifaioglu.ce@gmail.com>
2019-02-06 21:57:31 +0000

    updated

--- bin/dream_challenge.py
+++ bin/dream_challenge.py
@@ -181,8 +181,8 @@ def train_networks(mod, comp_feat, tar_feat, comp_hidden_lst, tar_hidden_lst, fc
                         print("loggedpred", -math.log10(10e-10*float(item.data[0])))
                         validation_predictions.append(-math.log10(10e-10*float(item.data[0])))
                     for item in val_labels:
-                        # validation_predictions.append(float(item.data[0]))
-                        validation_predictions.append(-math.log10(10e-10*float(item.data[0])))
+                        # validation_labels.append(float(item.data[0]))
+                        validation_labels.append(-math.log10(10e-10*float(item.data[0])))
 
             rmse_score = rmse(np.asarray(validation_labels), np.asarray(
                 validation_predictions))
