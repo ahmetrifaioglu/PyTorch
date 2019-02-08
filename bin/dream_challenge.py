7edc42afe2dc2196b3b0e6aa803bc8ce553c016f
ahmetrifaioglu <ahmetrifaioglu.ce@gmail.com>
2019-02-08 10:58:27 +0000

    updated

--- bin/dream_challenge.py
+++ bin/dream_challenge.py
@@ -159,7 +159,7 @@ def train_networks(mod, comp_feat, tar_feat, comp_hidden_lst, tar_hidden_lst, fc
                         weights.append([3,1])
 
                 weights = torch.FloatTensor(weights)
-                print(len(weights), len(labels))
+                # print(len(weights), len(labels))
                 loss = None
                 if regression_classifier=="r":
                     loss = criterion(y_pred.squeeze(), labels)
