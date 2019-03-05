7f051c16a68ecb7dfb75fe56be52a2466d41cb2d
ahmetrifaioglu <ahmetrifaioglu.ce@gmail.com>
2019-03-05 11:28:20 +0000

    updated

--- bin/last_rnn_out_1.sh
+++ bin/last_rnn_out_1.sh
@@ -1,117 +1,233 @@
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_1.out "python last_rnn_out_experiment_1_1.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_2.out "python last_rnn_out_experiment_1_2.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_3.out "python last_rnn_out_experiment_1_3.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_4.out "python last_rnn_out_experiment_1_4.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_5.out "python last_rnn_out_experiment_1_5.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_6.out "python last_rnn_out_experiment_1_6.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_7.out "python last_rnn_out_experiment_1_7.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_8.out "python last_rnn_out_experiment_1_8.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_9.out "python last_rnn_out_experiment_1_9.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_10.out "python last_rnn_out_experiment_1_10.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_11.out "python last_rnn_out_experiment_1_11.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_12.out "python last_rnn_out_experiment_1_12.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_13.out "python last_rnn_out_experiment_1_13.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_14.out "python last_rnn_out_experiment_1_14.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_15.out "python last_rnn_out_experiment_1_15.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_16.out "python last_rnn_out_experiment_1_16.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_17.out "python last_rnn_out_experiment_1_17.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_18.out "python last_rnn_out_experiment_1_18.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_19.out "python last_rnn_out_experiment_1_19.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_20.out "python last_rnn_out_experiment_1_20.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_21.out "python last_rnn_out_experiment_1_21.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_22.out "python last_rnn_out_experiment_1_22.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_23.out "python last_rnn_out_experiment_1_23.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_24.out "python last_rnn_out_experiment_1_24.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_25.out "python last_rnn_out_experiment_1_25.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_26.out "python last_rnn_out_experiment_1_26.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_27.out "python last_rnn_out_experiment_1_27.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_28.out "python last_rnn_out_experiment_1_28.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_29.out "python last_rnn_out_experiment_1_29.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_30.out "python last_rnn_out_experiment_1_30.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_31.out "python last_rnn_out_experiment_1_31.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_32.out "python last_rnn_out_experiment_1_32.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_33.out "python last_rnn_out_experiment_1_33.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_34.out "python last_rnn_out_experiment_1_34.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_35.out "python last_rnn_out_experiment_1_35.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_36.out "python last_rnn_out_experiment_1_36.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_37.out "python last_rnn_out_experiment_1_37.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_38.out "python last_rnn_out_experiment_1_38.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_39.out "python last_rnn_out_experiment_1_39.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_40.out "python last_rnn_out_experiment_1_40.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_41.out "python last_rnn_out_experiment_1_41.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_42.out "python last_rnn_out_experiment_1_42.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_43.out "python last_rnn_out_experiment_1_43.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_44.out "python last_rnn_out_experiment_1_44.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_45.out "python last_rnn_out_experiment_1_45.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_46.out "python last_rnn_out_experiment_1_46.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_47.out "python last_rnn_out_experiment_1_47.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_48.out "python last_rnn_out_experiment_1_48.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_49.out "python last_rnn_out_experiment_1_49.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_50.out "python last_rnn_out_experiment_1_50.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_51.out "python last_rnn_out_experiment_1_51.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_52.out "python last_rnn_out_experiment_1_52.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_53.out "python last_rnn_out_experiment_1_53.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_54.out "python last_rnn_out_experiment_1_54.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_55.out "python last_rnn_out_experiment_1_55.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_56.out "python last_rnn_out_experiment_1_56.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_57.out "python last_rnn_out_experiment_1_57.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_58.out "python last_rnn_out_experiment_1_58.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_59.out "python last_rnn_out_experiment_1_59.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_60.out "python last_rnn_out_experiment_1_60.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_61.out "python last_rnn_out_experiment_1_61.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_62.out "python last_rnn_out_experiment_1_62.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_63.out "python last_rnn_out_experiment_1_63.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_64.out "python last_rnn_out_experiment_1_64.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_65.out "python last_rnn_out_experiment_1_65.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_66.out "python last_rnn_out_experiment_1_66.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_67.out "python last_rnn_out_experiment_1_67.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_68.out "python last_rnn_out_experiment_1_68.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_69.out "python last_rnn_out_experiment_1_69.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_70.out "python last_rnn_out_experiment_1_70.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_71.out "python last_rnn_out_experiment_1_71.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_72.out "python last_rnn_out_experiment_1_72.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_73.out "python last_rnn_out_experiment_1_73.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_74.out "python last_rnn_out_experiment_1_74.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_75.out "python last_rnn_out_experiment_1_75.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_76.out "python last_rnn_out_experiment_1_76.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_77.out "python last_rnn_out_experiment_1_77.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_78.out "python last_rnn_out_experiment_1_78.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_79.out "python last_rnn_out_experiment_1_79.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_80.out "python last_rnn_out_experiment_1_80.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_81.out "python last_rnn_out_experiment_1_81.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_82.out "python last_rnn_out_experiment_1_82.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_83.out "python last_rnn_out_experiment_1_83.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_84.out "python last_rnn_out_experiment_1_84.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_85.out "python last_rnn_out_experiment_1_85.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_86.out "python last_rnn_out_experiment_1_86.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_87.out "python last_rnn_out_experiment_1_87.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_88.out "python last_rnn_out_experiment_1_88.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_89.out "python last_rnn_out_experiment_1_89.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_90.out "python last_rnn_out_experiment_1_90.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_91.out "python last_rnn_out_experiment_1_91.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_92.out "python last_rnn_out_experiment_1_92.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_93.out "python last_rnn_out_experiment_1_93.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_94.out "python last_rnn_out_experiment_1_94.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_95.out "python last_rnn_out_experiment_1_95.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_96.out "python last_rnn_out_experiment_1_96.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_97.out "python last_rnn_out_experiment_1_97.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_98.out "python last_rnn_out_experiment_1_98.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_99.out "python last_rnn_out_experiment_1_99.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_100.out "python last_rnn_out_experiment_1_100.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_101.out "python last_rnn_out_experiment_1_101.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_102.out "python last_rnn_out_experiment_1_102.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_103.out "python last_rnn_out_experiment_1_103.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_104.out "python last_rnn_out_experiment_1_104.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_105.out "python last_rnn_out_experiment_1_105.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_106.out "python last_rnn_out_experiment_1_106.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_107.out "python last_rnn_out_experiment_1_107.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_108.out "python last_rnn_out_experiment_1_108.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_109.out "python last_rnn_out_experiment_1_109.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_110.out "python last_rnn_out_experiment_1_110.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_111.out "python last_rnn_out_experiment_1_111.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_112.out "python last_rnn_out_experiment_1_112.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_113.out "python last_rnn_out_experiment_1_113.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_114.out "python last_rnn_out_experiment_1_114.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_115.out "python last_rnn_out_experiment_1_115.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_116.out "python last_rnn_out_experiment_1_116.py"
+sleep 1
 bsub -q research-rh74 -P gpu -gpu - -M 10240 -o last_rnn_out_experiment_117.out "python last_rnn_out_experiment_1_117.py"
