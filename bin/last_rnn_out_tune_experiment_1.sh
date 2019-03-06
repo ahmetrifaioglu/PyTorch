65e55c763ec874d40664d10ce9ab802e1421c7d2
ahmetrifaioglu <ahmetrifaioglu.ce@gmail.com>
2019-03-06 09:33:21 +0000

    updaed

---
+++ bin/last_rnn_out_tune_experiment_1.sh
@@ -0,0 +1,59 @@
+bsub -q research-rh74 -P gpu -gpu - -M 20480 -o last_rnn_out_tune_experiment_1_1.out "python rnn_playground_tune.py"
+sleep 1
+bsub -q research-rh74 -P gpu -gpu - -M 20480 -o last_rnn_out_tune_experiment_1_2.out "python rnn_playground_tune.py"
+sleep 1
+bsub -q research-rh74 -P gpu -gpu - -M 20480 -o last_rnn_out_tune_experiment_1_3.out "python rnn_playground_tune.py"
+sleep 1
+bsub -q research-rh74 -P gpu -gpu - -M 20480 -o last_rnn_out_tune_experiment_1_4.out "python rnn_playground_tune.py"
+sleep 1
+bsub -q research-rh74 -P gpu -gpu - -M 20480 -o last_rnn_out_tune_experiment_1_5.out "python rnn_playground_tune.py"
+sleep 1
+bsub -q research-rh74 -P gpu -gpu - -M 20480 -o last_rnn_out_tune_experiment_1_6.out "python rnn_playground_tune.py"
+sleep 1
+bsub -q research-rh74 -P gpu -gpu - -M 20480 -o last_rnn_out_tune_experiment_1_7.out "python rnn_playground_tune.py"
+sleep 1
+bsub -q research-rh74 -P gpu -gpu - -M 20480 -o last_rnn_out_tune_experiment_1_8.out "python rnn_playground_tune.py"
+sleep 1
+bsub -q research-rh74 -P gpu -gpu - -M 20480 -o last_rnn_out_tune_experiment_1_9.out "python rnn_playground_tune.py"
+sleep 1
+bsub -q research-rh74 -P gpu -gpu - -M 20480 -o last_rnn_out_tune_experiment_1_10.out "python rnn_playground_tune.py"
+sleep 1
+bsub -q research-rh74 -P gpu -gpu - -M 20480 -o last_rnn_out_tune_experiment_1_11.out "python rnn_playground_tune.py"
+sleep 1
+bsub -q research-rh74 -P gpu -gpu - -M 20480 -o last_rnn_out_tune_experiment_1_12.out "python rnn_playground_tune.py"
+sleep 1
+bsub -q research-rh74 -P gpu -gpu - -M 20480 -o last_rnn_out_tune_experiment_1_13.out "python rnn_playground_tune.py"
+sleep 1
+bsub -q research-rh74 -P gpu -gpu - -M 20480 -o last_rnn_out_tune_experiment_1_14.out "python rnn_playground_tune.py"
+sleep 1
+bsub -q research-rh74 -P gpu -gpu - -M 20480 -o last_rnn_out_tune_experiment_1_15.out "python rnn_playground_tune.py"
+sleep 1
+bsub -q research-rh74 -P gpu -gpu - -M 20480 -o last_rnn_out_tune_experiment_1_16.out "python rnn_playground_tune.py"
+sleep 1
+bsub -q research-rh74 -P gpu -gpu - -M 20480 -o last_rnn_out_tune_experiment_1_17.out "python rnn_playground_tune.py"
+sleep 1
+bsub -q research-rh74 -P gpu -gpu - -M 20480 -o last_rnn_out_tune_experiment_1_18.out "python rnn_playground_tune.py"
+sleep 1
+bsub -q research-rh74 -P gpu -gpu - -M 20480 -o last_rnn_out_tune_experiment_1_19.out "python rnn_playground_tune.py"
+sleep 1
+bsub -q research-rh74 -P gpu -gpu - -M 20480 -o last_rnn_out_tune_experiment_1_20.out "python rnn_playground_tune.py"
+sleep 1
+bsub -q research-rh74 -P gpu -gpu - -M 20480 -o last_rnn_out_tune_experiment_1_21.out "python rnn_playground_tune.py"
+sleep 1
+bsub -q research-rh74 -P gpu -gpu - -M 20480 -o last_rnn_out_tune_experiment_1_22.out "python rnn_playground_tune.py"
+sleep 1
+bsub -q research-rh74 -P gpu -gpu - -M 20480 -o last_rnn_out_tune_experiment_1_23.out "python rnn_playground_tune.py"
+sleep 1
+bsub -q research-rh74 -P gpu -gpu - -M 20480 -o last_rnn_out_tune_experiment_1_24.out "python rnn_playground_tune.py"
+sleep 1
+bsub -q research-rh74 -P gpu -gpu - -M 20480 -o last_rnn_out_tune_experiment_1_25.out "python rnn_playground_tune.py"
+sleep 1
+bsub -q research-rh74 -P gpu -gpu - -M 20480 -o last_rnn_out_tune_experiment_1_26.out "python rnn_playground_tune.py"
+sleep 1
+bsub -q research-rh74 -P gpu -gpu - -M 20480 -o last_rnn_out_tune_experiment_1_27.out "python rnn_playground_tune.py"
+sleep 1
+bsub -q research-rh74 -P gpu -gpu - -M 20480 -o last_rnn_out_tune_experiment_1_28.out "python rnn_playground_tune.py"
+sleep 1
+bsub -q research-rh74 -P gpu -gpu - -M 20480 -o last_rnn_out_tune_experiment_1_29.out "python rnn_playground_tune.py"
+sleep 1
+bsub -q research-rh74 -P gpu -gpu - -M 20480 -o last_rnn_out_tune_experiment_1_30.out "python rnn_playground_tune	py"
