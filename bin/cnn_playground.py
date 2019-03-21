e09e4d17496e94d479d6786db154970b53d5d778
ahmetrifaioglu <ahmetrifaioglu.ce@gmail.com>
2019-03-21 15:42:10 +0000

    updated

--- bin/cnn_playground.py
+++ bin/cnn_playground.py
@@ -18,7 +18,7 @@ from sklearn.metrics import f1_score, accuracy_score
 from sklearn import preprocessing,metrics
 import sklearn
 from dream_challenge_PINN_models import FC_PINNModel_2_2_2, FC_PINNModel_2_2_2_Modules, FC_PINNModel_2_3_2_Modules, FC_PINNModel_3_5_2_Modules#, FC_PINNModel_4_4_2,  FC_PINNModel_3_3_2
-from dream_challenge_data_processing import TrainingValidationShuffledDataLoader, get_nfold_data_loader_dict,get_training_validation_data_loaders_for_cnn
+# from dream_challenge_data_processing import TrainingValidationShuffledDataLoader, get_nfold_data_loader_dict,get_training_validation_data_loaders_for_cnn
 from rnn_playground_models import CompFCNNTarRNN, CompFCNNTarCNN
 from emetrics import r_squared_error, get_rm2, squared_error_zero, get_k, get_cindex, get_aupr
 from cnn_data_processing import get_cnn_test_val_folds_train_data_loader
@@ -38,10 +38,12 @@ def train_networks(comp_feature_list, tar_feature_list, comp_hidden_lst, fc1, fc
     else:
         print("CPU is available on this device!")
     davis_prot_fl_path = "../trainingFiles/DeepDTA/helper_files/davis_prots.fasta"
-    loader_fold_dict, number_of_comp_features, number_of_target_features = get_training_validation_data_loaders_for_cnn(1, 32, ["ecfp4"], ["sequencematrix500"], "davis_comp_targ_affinity.csv", davis_prot_fl_path, "r")
+    print("1")
+    #loader_fold_dict, number_of_comp_features, number_of_target_features = get_cnn_test_val_folds_train_data_loader(1, 32, ["ecfp4"], ["sequencematrix500"], "davis_comp_targ_affinity.csv", davis_prot_fl_path, "r")
+    loader_fold_dict, test_loader = get_cnn_test_val_folds_train_data_loader()
+    print("2")
     #original_number_of_comp_features = int(number_of_comp_features)
     #original_number_of_target_features = int(number_of_target_features)
-    loader_fold_dict, test_loader = get_cnn_test_val_folds_train_data_loader()
 
     # print(original_number_of_comp_features, original_number_of_target_features)
 
@@ -49,7 +51,7 @@ def train_networks(comp_feature_list, tar_feature_list, comp_hidden_lst, fc1, fc
         train_loader, valid_loader = loader_fold_dict[fold]
         print("FOLD : {}".format(fold + 1))
         #number_of_comp_features = original_number_of_comp_features
-        model = CompFCNNTarCNN(number_of_comp_features, 1024, 512, 256, 256, drop_prob=0.5).to(device)
+        model = CompFCNNTarCNN(1024, 1024, 512, 256, 256, drop_prob=0.5).to(device)
 
         optimizer = torch.optim.Adam(model.parameters(), lr=learn_rate)
         criterion = torch.nn.MSELoss()
