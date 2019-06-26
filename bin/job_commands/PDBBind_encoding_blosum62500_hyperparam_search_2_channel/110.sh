70d7904eb27ec412073e78faedc51d2f0258322a
ahmetrifaioglu <ahmetrifaioglu.ce@gmail.com>
2019-06-26 13:56:47 +0100

    updated

---
+++ bin/job_commands/PDBBind_encoding_blosum62500_hyperparam_search_2_channel/110.sh
@@ -0,0 +1,20 @@
+python ../../cnn_playground.py 1024_1024 1024 256_128512_256 0.001 32 PDBBind ecfp4 sequencematrix500_blosum62500 1 CompFCNNTarCNN2 0.3
+sleep 1
+python ../../cnn_playground.py 1024_1024 1024 256_128512_256 0.001 32 PDBBind ecfp4 sequencematrix500_blosum62500 1 CompFCNNTarCNN2 0.5
+sleep 1
+python ../../cnn_playground.py 512_512 1024 256_128512_256 0.01 32 PDBBind ecfp4 sequencematrix500_blosum62500 1 CompFCNNTarCNN2 0.2
+sleep 1
+python ../../cnn_playground.py 512_512 1024 256_128512_256 0.01 32 PDBBind ecfp4 sequencematrix500_blosum62500 1 CompFCNNTarCNN2 0.3
+sleep 1
+python ../../cnn_playground.py 512_512 1024 256_128512_256 0.01 32 PDBBind ecfp4 sequencematrix500_blosum62500 1 CompFCNNTarCNN2 0.5
+sleep 1
+python ../../cnn_playground.py 1024_512 1024 256_128512_256 0.01 32 PDBBind ecfp4 sequencematrix500_blosum62500 1 CompFCNNTarCNN2 0.2
+sleep 1
+python ../../cnn_playground.py 1024_512 1024 256_128512_256 0.01 32 PDBBind ecfp4 sequencematrix500_blosum62500 1 CompFCNNTarCNN2 0.3
+sleep 1
+python ../../cnn_playground.py 1024_512 1024 256_128512_256 0.01 32 PDBBind ecfp4 sequencematrix500_blosum62500 1 CompFCNNTarCNN2 0.5
+sleep 1
+python ../../cnn_playground.py 1024_256 1024 256_128512_256 0.01 32 PDBBind ecfp4 sequencematrix500_blosum62500 1 CompFCNNTarCNN2 0.2
+sleep 1
+python ../../cnn_playground.py 1024_256 1024 256_128512_256 0.01 32 PDBBind ecfp4 sequencematrix500_blosum62500 1 CompFCNNTarCNN2 0.3
+sleep 1
---
+++ bin/job_commands/PDBBind_encoding_blosum62500_hyperparam_search_2_channel/69.sh
@@ -0,0 +1,20 @@
+python ../../cnn_playground.py 1024_256 128 1024_512 0.0001 32 PDBBind ecfp4 sequencematrix500_blosum62500 1 CompFCNNTarCNN2 0.5
+sleep 1
+python ../../cnn_playground.py 1024_1024 128 1024_512 0.0001 32 PDBBind ecfp4 sequencematrix500_blosum62500 1 CompFCNNTarCNN2 0.2
+sleep 1
+python ../../cnn_playground.py 1024_1024 128 1024_512 0.0001 32 PDBBind ecfp4 sequencematrix500_blosum62500 1 CompFCNNTarCNN2 0.3
+sleep 1
+python ../../cnn_playground.py 1024_1024 128 1024_512 0.0001 32 PDBBind ecfp4 sequencematrix500_blosum62500 1 CompFCNNTarCNN2 0.5
+sleep 1
+python ../../cnn_playground.py 512_512 128 1024_512 0.005 32 PDBBind ecfp4 sequencematrix500_blosum62500 1 CompFCNNTarCNN2 0.2
+sleep 1
+python ../../cnn_playground.py 512_512 128 1024_512 0.005 32 PDBBind ecfp4 sequencematrix500_blosum62500 1 CompFCNNTarCNN2 0.3
+sleep 1
+python ../../cnn_playground.py 512_512 128 1024_512 0.005 32 PDBBind ecfp4 sequencematrix500_blosum62500 1 CompFCNNTarCNN2 0.5
+sleep 1
+python ../../cnn_playground.py 1024_512 128 1024_512 0.005 32 PDBBind ecfp4 sequencematrix500_blosum62500 1 CompFCNNTarCNN2 0.2
+sleep 1
+python ../../cnn_playground.py 1024_512 128 1024_512 0.005 32 PDBBind ecfp4 sequencematrix500_blosum62500 1 CompFCNNTarCNN2 0.3
+sleep 1
+python ../../cnn_playground.py 1024_512 128 1024_512 0.005 32 PDBBind ecfp4 sequencematrix500_blosum62500 1 CompFCNNTarCNN2 0.5
+sleep 1
