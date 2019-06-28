d3fc9da5f06f59c0137e9b5b956c906dc344e833
ahmetrifaioglu <ahmetrifaioglu.ce@gmail.com>
2019-06-28 22:14:22 +0100

    updated

--- bin/cnn_playground.py
+++ bin/cnn_playground.py
@@ -27,7 +27,7 @@ warnings.filterwarnings(action='ignore')
 cwd = os.getcwd()
 project_file_path = "{}PyTorch".format(cwd.split("PyTorch")[0])
 
-n_epoch = 100
+n_epoch = 75
 num_of_folds = 5
 
 def get_model(model_name, tar_feature_list, num_of_com_features, tar_num_of_last_neurons, comp_hidden_first, comp_hidden_second, fc1, fc2, dropout):
@@ -212,8 +212,8 @@ def train_networks(training_dataset, comp_feature_list, tar_feature_list, comp_h
                     #print(len(test_fold_epoch_results[-1]))
                     mse_results = [epoch_score_dict["MSE"] for epoch_score_dict in test_fold_epoch_results[-1]]
 
-                    #if min(mse_results)>= 0.30:
-                    #    sys.exit("Terminating training since minimum MSE is higher than the threshold!")
+                    if min(mse_results)>= 0.28:
+                        sys.exit("Terminating training since minimum MSE is higher than the threshold!")
 
     if not os.path.exists("{}/result_files/{}".format(project_file_path, experiment_name)):
         subprocess.call("mkdir {}".format("{}/result_files/{}".format(project_file_path, experiment_name)), shell=True)
