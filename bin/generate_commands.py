545a83f93c76c682af65f99b6e42c7bfae6abb8d
ahmetrifaioglu <ahmetrifaioglu.ce@gmail.com>
2019-07-10 16:55:17 +0100

    updated

--- bin/generate_commands.py
+++ bin/generate_commands.py
@@ -484,8 +484,8 @@ def generate_protein_cnn_commands(job_group_name, num_of_jobs_at_each_group):
                                             job_number += 1
                                             all_job_submission_fl.write("chmod +x ./{}_{}.sh\n".format(job_number, num_of_jobs_at_each_group))
                                             all_job_submission_fl.write(
-                                                "./{}.sh > ../../../result_files/{}/{}.out\n".format((job_number), job_group_name,
-                                                                                        (job_number)))
+                                                "./{}_{}.sh > ../../../result_files/{}/{}_{}.out\n".format((job_number), num_of_jobs_at_each_group, job_group_name,
+                                                                                        (job_number), num_of_jobs_at_each_group))
                                             # all_job_submission_fl.write("bsub -g /my_gpu_group -q research-rh74 -P gpu -gpu \"num=1:j_exclusive=yes\" -M 5120 -R 'rusage[mem=5120]' -o ../../../log_files/{}/{}_{}.out \"./{}_{}.sh\"\n".format(job_group_name, job_number, num_of_jobs_at_each_group, job_number, num_of_jobs_at_each_group))
                                             # all_job_submission_fl.write(
                                             #    "bsub -q research-rh74 -M 5120 -R 'rusage[mem=5120]' -o ../../../log_files/{}/{}.out \"./{}.sh\"\n".format(
@@ -513,7 +513,7 @@ def generate_protein_cnn_commands(job_group_name, num_of_jobs_at_each_group):
     if len(temp_group_job_list)!=0:
         job_fl = open("./{}/{}_{}.sh".format(job_folder_path, job_number + 1, num_of_jobs_at_each_group), "w")
         all_job_submission_fl.write("chmod +x ./{}_{}.sh\n".format(job_number+1, num_of_jobs_at_each_group))
-        all_job_submission_fl.write("./{}.sh > ../../../result_files/{}/{}.out\n".format((job_number+1), job_group_name, (job_number+1)))
+        all_job_submission_fl.write("./{}_{}.sh > ../../../result_files/{}/{}_{}.out\n".format((job_number+1), num_of_jobs_at_each_group, job_group_name, (job_number+1), num_of_jobs_at_each_group))
 
         #all_job_submission_fl.write(
         #    "bsub -g /my_gpu_group -q research-rh74 -P gpu -gpu \"num=1:j_exclusive=yes\" -M 5120 -R 'rusage[mem=5120]' -o ../../../log_files/{}/{}.out \"./{}.sh\"\n".format(
