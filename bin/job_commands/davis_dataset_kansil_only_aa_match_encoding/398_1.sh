9d361b3108c59cb6280ee6d03d6d872bac923e94
ahmetrifaioglu <ahmetrifaioglu.ce@gmail.com>
2019-07-10 16:57:00 +0100

    updated

---
+++ bin/job_commands/davis_dataset_kansil_only_aa_match_encoding/398_1.sh
@@ -0,0 +1,2 @@
+python ../../cnn_playground.py 1024_512 1024 1024_512 0.005 32 DeepDTA_davis ecfp4 sequencematrix500 1 CompFCNNTarCNNModule2Layers 0.1 davis_dataset_kansil_only_aa_match_encoding
+sleep 1
