93bd98c982049536743f4b0debab444521ac198c
ahmetrifaioglu <ahmetrifaioglu.ce@gmail.com>
2019-07-14 22:55:10 +0100

    updated

---
+++ bin/job_commands/DavisDataset_filtered_all_encodings_varying_channel_2/87_20.sh
@@ -0,0 +1,16 @@
+python ../../cnn_playground.py 1024_1024 1024 1024_512 0.001 32 DeepDTA_davis ecfp4 sequencematrix500 1 CompFCNNTarCNNModule2Layers 0.2 DavisDataset_filtered_all_encodings_varying_channel_2
+sleep 1
+python ../../cnn_playground.py 1024_1024 1024 1024_512 0.001 32 DeepDTA_davis ecfp4 sequencematrix500_blosum62LEQ500 1 CompFCNNTarCNNModule2Layers 0.2 DavisDataset_filtered_all_encodings_varying_channel_2
+sleep 1
+python ../../cnn_playground.py 1024_1024 1024 1024_512 0.001 32 DeepDTA_davis ecfp4 sequencematrix500_SIMK990101LEQ500 1 CompFCNNTarCNNModule2Layers 0.2 DavisDataset_filtered_all_encodings_varying_channel_2
+sleep 1
+python ../../cnn_playground.py 1024_1024 1024 1024_512 0.001 32 DeepDTA_davis ecfp4 sequencematrix500_blosum62LEQ500_SIMK990101LEQ500 1 CompFCNNTarCNNModule2Layers 0.2 DavisDataset_filtered_all_encodings_varying_channel_2
+sleep 1
+python ../../cnn_playground.py 1024_1024 1024 1024_512 0.001 32 DeepDTA_davis ecfp4 sequencematrix500 1 CompFCNNTarCNN4LayersStride 0.2 DavisDataset_filtered_all_encodings_varying_channel_2
+sleep 1
+python ../../cnn_playground.py 1024_1024 1024 1024_512 0.001 32 DeepDTA_davis ecfp4 sequencematrix500_blosum62LEQ500 1 CompFCNNTarCNN4LayersStride 0.2 DavisDataset_filtered_all_encodings_varying_channel_2
+sleep 1
+python ../../cnn_playground.py 1024_1024 1024 1024_512 0.001 32 DeepDTA_davis ecfp4 sequencematrix500_SIMK990101LEQ500 1 CompFCNNTarCNN4LayersStride 0.2 DavisDataset_filtered_all_encodings_varying_channel_2
+sleep 1
+python ../../cnn_playground.py 1024_1024 1024 1024_512 0.001 32 DeepDTA_davis ecfp4 sequencematrix500_blosum62LEQ500_SIMK990101LEQ500 1 CompFCNNTarCNN4LayersStride 0.2 DavisDataset_filtered_all_encodings_varying_channel_2
+sleep 1
