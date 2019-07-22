da322ce67c6dec8d5f2dc73c6e250bc1834c2dec
ahmetrifaioglu <ahmetrifaioglu.ce@gmail.com>
2019-07-22 12:44:45 +0100

    updated

--- bin/static_file_generation_modules.py
+++ bin/static_file_generation_modules.py
@@ -250,7 +250,7 @@ def save_all_flattened_sequence_matrices(fasta_fl_path, size, aaindex_enconding=
 # save_all_flattened_sequence_matrices("../trainingFiles/PDBBind_Refined/helper_files/targets.fasta", 500, "KESO980101.txt")
 
 # python static_file_generation_modules.py > ../trainingFiles/PDBBind_Refined/target_feature_vectors/GRAR740104LEQ500.tsv
-# save_all_flattened_sequence_matrices("../trainingFiles/PDBBind_Refined/helper_files/targets.fasta", 500, "GRAR740104.txt")
+save_all_flattened_sequence_matrices("../trainingFiles/PDBBind_Refined/helper_files/targets.fasta", 500, "GRAR740104.txt")
 
 
 # Components-smiles-stereo-oe.smi and Components-inchi.ich.txt were downloaded from  http://ligand-expo.rcsb.org/ld-download.html
