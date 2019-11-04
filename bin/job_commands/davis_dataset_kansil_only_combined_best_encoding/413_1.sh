566c83d9e7c6aa53020291fd415ee958388c26b7
ahmetrifaioglu <ahmetrifaioglu.ce@gmail.com>
2019-11-04 16:24:55 +0300

    updated

---
+++ bin/job_commands/davis_dataset_kansil_only_combined_best_encoding/413_1.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+python ../../cnn_playground.py 1024_256 1024 512_256 0.0001 32 Davis ecfp4 sequencematrix500_ZHAC000103LEQ500_GRAR740104LEQ500_SIMK990101LEQ500_blosum62LEQ500 1 CompFCNNTarCNNModuleInception 0.1 davis_dataset_kansil_only_combined_best_encoding
+sleep 1
