4d23437b749a97046af3b0e0aaadbb19d96eb9e5
ahmetrifaioglu <ahmetrifaioglu.ce@gmail.com>
2020-01-05 10:21:01 +0300

    updated

--- bin/job_commands/pdbbind_refined_dataset_ebi_1000_combined_best_encodings/147_1.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-python ../../cnn_playground.py 1024_512 128 1024_512 0.0001 32 PDBBind_Refined ecfp4 sequencematrix1000_ZHAC000103LEQ1000_GRAR740104LEQ1000_SIMK990101LEQ1000_blosum62LEQ1000 1 CompFCNNTarCNNModuleInception 0.1 pdbbind_refined_dataset_ebi_1000_combined_best_encodings
-sleep 1
--- bin/job_commands/pdbbind_refined_dataset_ebi_1000_combined_best_encodings/312_1.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-python ../../cnn_playground.py 1024_1024 512 256_128 0.01 32 PDBBind_Refined ecfp4 sequencematrix1000_ZHAC000103LEQ1000_GRAR740104LEQ1000_SIMK990101LEQ1000_blosum62LEQ1000 1 CompFCNNTarCNNModuleInception 0.25 pdbbind_refined_dataset_ebi_1000_combined_best_encodings
-sleep 1
