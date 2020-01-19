aece610b64efe5dd8adbbabf4f8518104606bfed
ahmetrifaioglu <ahmetrifaioglu.ce@gmail.com>
2020-01-19 23:40:19 +0300

    updatedcd

--- bin/generate_commands.py
+++ bin/generate_commands.py
@@ -599,7 +599,7 @@ def generate_protein_cnn_commands(job_group_name, num_of_jobs_at_each_group):
 # generate_protein_cnn_commands("pdbbind_refined_dataset_kansil_combined_best_encodings", 1)
 # generate_protein_cnn_commands("davis_dataset_kansil_only_combined_best_encoding", 1)
 # generate_protein_cnn_commands("davis_dataset_ebi_gpu_only_combined_best_encoding", 5)
-# generate_protein_cnn_commands("kinome_dataset_ebi_gpu_only_combined_best_encoding", 5)
+generate_protein_cnn_commands("kinome_dataset_ebi_gpu_only_combined_best_encoding", 5)
 
 # generate_protein_cnn_commands("pdbbind_refined_dataset_ebi_1000_combined_best_encodings", 1)
 
--- bin/job_commands/kinome_dataset_ebi_gpu_only_combined_best_encoding/29_5.sh
+++ bin/job_commands/kinome_dataset_ebi_gpu_only_combined_best_encoding/29_5.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-python ../../cnn_playground.py 1024_256 512 1024_1024 0.01 32 kinome ecfp4 sequencematrix500_ZHAC000103LEQ500_GRAR740104LEQ500_SIMK990101LEQ500_blosum62LEQ500 1 CompFCNNTarCNNModuleInception 0.1 kinome_dataset_ebi_gpu_only_combined_best_encoding
+python ../../cnn_playground.py 1024_256 512 1024_1024 0.01 32 kinome ecfp4 sequencematrix1000_ZHAC000103LEQ1000_GRAR740104LEQ1000_SIMK990101LEQ1000_blosum62LEQ1000 1 CompFCNNTarCNNModuleInception 0.1 kinome_dataset_ebi_gpu_only_combined_best_encoding
 sleep 1
-python ../../cnn_playground.py 1024_256 512 1024_1024 0.01 32 kinome ecfp4 sequencematrix500_ZHAC000103LEQ500_GRAR740104LEQ500_SIMK990101LEQ500_blosum62LEQ500 1 CompFCNNTarCNNModuleInception 0.25 kinome_dataset_ebi_gpu_only_combined_best_encoding
+python ../../cnn_playground.py 1024_256 512 1024_1024 0.01 32 kinome ecfp4 sequencematrix1000_ZHAC000103LEQ1000_GRAR740104LEQ1000_SIMK990101LEQ1000_blosum62LEQ1000 1 CompFCNNTarCNNModuleInception 0.25 kinome_dataset_ebi_gpu_only_combined_best_encoding
 sleep 1
-python ../../cnn_playground.py 1024_1024 512 1024_1024 0.01 32 kinome ecfp4 sequencematrix500_ZHAC000103LEQ500_GRAR740104LEQ500_SIMK990101LEQ500_blosum62LEQ500 1 CompFCNNTarCNNModuleInception 0.1 kinome_dataset_ebi_gpu_only_combined_best_encoding
+python ../../cnn_playground.py 1024_1024 512 1024_1024 0.01 32 kinome ecfp4 sequencematrix1000_ZHAC000103LEQ1000_GRAR740104LEQ1000_SIMK990101LEQ1000_blosum62LEQ1000 1 CompFCNNTarCNNModuleInception 0.1 kinome_dataset_ebi_gpu_only_combined_best_encoding
 sleep 1
-python ../../cnn_playground.py 1024_1024 512 1024_1024 0.01 32 kinome ecfp4 sequencematrix500_ZHAC000103LEQ500_GRAR740104LEQ500_SIMK990101LEQ500_blosum62LEQ500 1 CompFCNNTarCNNModuleInception 0.25 kinome_dataset_ebi_gpu_only_combined_best_encoding
+python ../../cnn_playground.py 1024_1024 512 1024_1024 0.01 32 kinome ecfp4 sequencematrix1000_ZHAC000103LEQ1000_GRAR740104LEQ1000_SIMK990101LEQ1000_blosum62LEQ1000 1 CompFCNNTarCNNModuleInception 0.25 kinome_dataset_ebi_gpu_only_combined_best_encoding
 sleep 1
-python ../../cnn_playground.py 512_512 1024 512_256 0.0001 32 kinome ecfp4 sequencematrix500_ZHAC000103LEQ500_GRAR740104LEQ500_SIMK990101LEQ500_blosum62LEQ500 1 CompFCNNTarCNNModuleInception 0.1 kinome_dataset_ebi_gpu_only_combined_best_encoding
+python ../../cnn_playground.py 512_512 1024 512_256 0.0001 32 kinome ecfp4 sequencematrix1000_ZHAC000103LEQ1000_GRAR740104LEQ1000_SIMK990101LEQ1000_blosum62LEQ1000 1 CompFCNNTarCNNModuleInception 0.1 kinome_dataset_ebi_gpu_only_combined_best_encoding
 sleep 1
