dade580d2ad1cba045855b7a07b5cc646c50712a
ahmetrifaioglu <ahmetrifaioglu.ce@gmail.com>
2020-01-23 15:20:18 +0300

    updated

--- bin/mbapred_test.py
+++ bin/mbapred_test.py
@@ -62,7 +62,7 @@ def load_model(model_fl):
     model = torch.load(model_fl)
     test_loader = get_cnn_test_data_loader("kinome", ["ecfp4"], "sequencematrix1000_ZHAC000103LEQ1000_GRAR740104LEQ1000_SIMK990101LEQ1000_blosum62LEQ1000".split("_"))
 
-
+    test_predictions, test_labels, test_all_comp_ids, test_all_tar_ids = [], [], [], []
     total_test_count = 0
     total_test_loss = 0.0
     model.eval()
