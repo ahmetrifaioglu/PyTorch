afa539bcd2ddfd7d45631f551dcaa7e0c5a66278
ahmetrifaioglu <ahmetrifaioglu.ce@gmail.com>
2020-03-13 10:08:22 +0300

    updated

--- bin/cnn_playground.py
+++ bin/cnn_playground.py
@@ -262,6 +262,8 @@ def train_networks(training_dataset, comp_feature_list, tar_feature_list, comp_h
 
                 if val_score_dict["MSE"]<best_val_fold_mse_score:
                     print("Model (Validation) is being saved.\tEpoch:{}\tOld MSE:{}\tNew MSE:{}".format(epoch, best_val_fold_mse_score, val_score_dict["MSE"]))
+                    best_val_fold_mse_score = val_score_dict["MSE"]
+
                     torch.save(model.state_dict(),
                                "{}/trained_models/kinome/kinome_best_val_{}_state_dict.pth".format(project_file_path, str_arguments))
                 if test_score_dict["MSE"]<best_test_fold_mse_score:
@@ -269,6 +271,7 @@ def train_networks(training_dataset, comp_feature_list, tar_feature_list, comp_h
                                                                                                   best_test_fold_mse_score,
                                                                                                         test_score_dict[
                                                                                                             "MSE"]))
+                    best_test_fold_mse_score = test_score_dict["MSE"]
                     torch.save(model.state_dict(),
                                "{}/trained_models/kinome/kinome_best_val_{}_state_dict.pth".format(project_file_path, str_arguments))
                 if epoch==n_epoch-1:
