2626e9a7ab4973cf658f9f95dc8f0b283af9cace
ahmetrifaioglu <ahmetrifaioglu.ce@gmail.com>
2020-05-01 08:29:23 +0300

    updated

---
+++ bin/job_commands/ACE2_model_training/ACE2_model_training.sh
@@ -0,0 +1,192 @@
+chmod +x ./1_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/1_5.out "./1_5.sh"
+chmod +x ./2_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/2_5.out "./2_5.sh"
+chmod +x ./3_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/3_5.out "./3_5.sh"
+chmod +x ./4_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/4_5.out "./4_5.sh"
+chmod +x ./5_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/5_5.out "./5_5.sh"
+chmod +x ./6_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/6_5.out "./6_5.sh"
+chmod +x ./7_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/7_5.out "./7_5.sh"
+chmod +x ./8_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/8_5.out "./8_5.sh"
+chmod +x ./9_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/9_5.out "./9_5.sh"
+chmod +x ./10_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/10_5.out "./10_5.sh"
+chmod +x ./11_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/11_5.out "./11_5.sh"
+chmod +x ./12_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/12_5.out "./12_5.sh"
+chmod +x ./13_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/13_5.out "./13_5.sh"
+chmod +x ./14_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/14_5.out "./14_5.sh"
+chmod +x ./15_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/15_5.out "./15_5.sh"
+chmod +x ./16_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/16_5.out "./16_5.sh"
+chmod +x ./17_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/17_5.out "./17_5.sh"
+chmod +x ./18_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/18_5.out "./18_5.sh"
+chmod +x ./19_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/19_5.out "./19_5.sh"
+chmod +x ./20_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/20_5.out "./20_5.sh"
+chmod +x ./21_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/21_5.out "./21_5.sh"
+chmod +x ./22_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/22_5.out "./22_5.sh"
+chmod +x ./23_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/23_5.out "./23_5.sh"
+chmod +x ./24_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/24_5.out "./24_5.sh"
+chmod +x ./25_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/25_5.out "./25_5.sh"
+chmod +x ./26_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/26_5.out "./26_5.sh"
+chmod +x ./27_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/27_5.out "./27_5.sh"
+chmod +x ./28_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/28_5.out "./28_5.sh"
+chmod +x ./29_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/29_5.out "./29_5.sh"
+chmod +x ./30_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/30_5.out "./30_5.sh"
+chmod +x ./31_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/31_5.out "./31_5.sh"
+chmod +x ./32_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/32_5.out "./32_5.sh"
+chmod +x ./33_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/33_5.out "./33_5.sh"
+chmod +x ./34_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/34_5.out "./34_5.sh"
+chmod +x ./35_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/35_5.out "./35_5.sh"
+chmod +x ./36_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/36_5.out "./36_5.sh"
+chmod +x ./37_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/37_5.out "./37_5.sh"
+chmod +x ./38_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/38_5.out "./38_5.sh"
+chmod +x ./39_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/39_5.out "./39_5.sh"
+chmod +x ./40_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/40_5.out "./40_5.sh"
+chmod +x ./41_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/41_5.out "./41_5.sh"
+chmod +x ./42_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/42_5.out "./42_5.sh"
+chmod +x ./43_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/43_5.out "./43_5.sh"
+chmod +x ./44_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/44_5.out "./44_5.sh"
+chmod +x ./45_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/45_5.out "./45_5.sh"
+chmod +x ./46_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/46_5.out "./46_5.sh"
+chmod +x ./47_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/47_5.out "./47_5.sh"
+chmod +x ./48_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/48_5.out "./48_5.sh"
+chmod +x ./49_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/49_5.out "./49_5.sh"
+chmod +x ./50_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/50_5.out "./50_5.sh"
+chmod +x ./51_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/51_5.out "./51_5.sh"
+chmod +x ./52_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/52_5.out "./52_5.sh"
+chmod +x ./53_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/53_5.out "./53_5.sh"
+chmod +x ./54_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/54_5.out "./54_5.sh"
+chmod +x ./55_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/55_5.out "./55_5.sh"
+chmod +x ./56_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/56_5.out "./56_5.sh"
+chmod +x ./57_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/57_5.out "./57_5.sh"
+chmod +x ./58_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/58_5.out "./58_5.sh"
+chmod +x ./59_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/59_5.out "./59_5.sh"
+chmod +x ./60_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/60_5.out "./60_5.sh"
+chmod +x ./61_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/61_5.out "./61_5.sh"
+chmod +x ./62_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/62_5.out "./62_5.sh"
+chmod +x ./63_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/63_5.out "./63_5.sh"
+chmod +x ./64_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/64_5.out "./64_5.sh"
+chmod +x ./65_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/65_5.out "./65_5.sh"
+chmod +x ./66_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/66_5.out "./66_5.sh"
+chmod +x ./67_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/67_5.out "./67_5.sh"
+chmod +x ./68_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/68_5.out "./68_5.sh"
+chmod +x ./69_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/69_5.out "./69_5.sh"
+chmod +x ./70_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/70_5.out "./70_5.sh"
+chmod +x ./71_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/71_5.out "./71_5.sh"
+chmod +x ./72_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/72_5.out "./72_5.sh"
+chmod +x ./73_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/73_5.out "./73_5.sh"
+chmod +x ./74_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/74_5.out "./74_5.sh"
+chmod +x ./75_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/75_5.out "./75_5.sh"
+chmod +x ./76_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/76_5.out "./76_5.sh"
+chmod +x ./77_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/77_5.out "./77_5.sh"
+chmod +x ./78_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/78_5.out "./78_5.sh"
+chmod +x ./79_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/79_5.out "./79_5.sh"
+chmod +x ./80_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/80_5.out "./80_5.sh"
+chmod +x ./81_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/81_5.out "./81_5.sh"
+chmod +x ./82_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/82_5.out "./82_5.sh"
+chmod +x ./83_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/83_5.out "./83_5.sh"
+chmod +x ./84_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/84_5.out "./84_5.sh"
+chmod +x ./85_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/85_5.out "./85_5.sh"
+chmod +x ./86_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/86_5.out "./86_5.sh"
+chmod +x ./87_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/87_5.out "./87_5.sh"
+chmod +x ./88_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/88_5.out "./88_5.sh"
+chmod +x ./89_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/89_5.out "./89_5.sh"
+chmod +x ./90_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/90_5.out "./90_5.sh"
+chmod +x ./91_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/91_5.out "./91_5.sh"
+chmod +x ./92_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/92_5.out "./92_5.sh"
+chmod +x ./93_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/93_5.out "./93_5.sh"
+chmod +x ./94_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/94_5.out "./94_5.sh"
+chmod +x ./95_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/95_5.out "./95_5.sh"
+chmod +x ./96_5.sh
+bsub -q research-rh74 -P gpu -M 20480 -R 'rusage[mem=20480]' -o ../../../result_files/ACE2_model_training/log_files/96_5.out "./96_5.sh"
